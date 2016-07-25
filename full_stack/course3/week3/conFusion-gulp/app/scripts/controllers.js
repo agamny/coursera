1eec984388d8f290c4ba0359aad7499238115b8b
Agam Singh <agam.singh@cummins.com>
2016-07-25 18:47:43 -0400

    Staging for week 3 exercise 2

---
+++ full_stack/course3/week3/conFusion-gulp/app/scripts/controllers.js
@@ -0,0 +1,108 @@
+'use strict';
+
+angular.module('confusionApp')
+
+    .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+
+            $scope.dishes= menuFactory.getDishes();            
+                        
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+    
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
+        .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
+                console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+
+            $scope.dish= menuFactory.getDish(3);
+            
+        }])
+
+        .controller('DishCommentController', function($scope) {
+            
+            $scope.commentIn = {author:"", rating:"", comment:"", date:"" };
+            
+            $scope.commentIn.rating = "5";
+    
+            //Step 1: Create a JavaScript object to hold the comment from the form
+            $scope.submitComment = function () {
+                
+                console.log($scope.commentIn);
+                
+                //Step 2: This is how you record the date
+                $scope.commentIn.date = new Date().toISOString();
+                
+                console.log($scope.commentIn.date);
+                console.log($scope.commentIn.comment);
+                console.log($scope.commentIn.author);
+                
+                // Step 3: Push your comment into the dish's comment array
+                $scope.dish.comments.push($scope.commentIn);
+                
+                //Step 4: reset your form to pristine
+                $scope.commentForm.$setPristine();
+                
+                //Step 5: reset your JavaScript object that holds your comment
+                //$scope.commentIn = {fullName:"", rating:"", commentText:"", commentDate:"" };
+                $scope.commentIn = {author:"", rating:"", comment:"", date:"" };
+                
+            }
+        })
+
+
+;
